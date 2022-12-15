0d7c1665431809338dbd20874b26c3f9bf418b4e
lci0502 <codls3717@gmail.com>
2022-12-15 19:33:31 +0900

    collectionView

---
+++ StockRank/StockRankViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by DALAMZ_CHAEIN on 2022/12/15.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
